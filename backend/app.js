5bf61936f889e3045e762adea226affd79dcb095
Wanderwize~ <wanderwwwize@yandex.ru>
2023-04-02 04:56:23 +0500

    Добавляет env переменные

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 
 const app = express();
 const mongoose = require("mongoose");
-
+куй
 const bodyParser = require("body-parser");
 const { errors } = require("celebrate");
 const { celebrate, Joi } = require("celebrate");
